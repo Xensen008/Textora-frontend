435bc3c606f3cf960cca1d81c24ea60254ace063
Xensen008 <arnabjyotikakati008@gmail.com>
2024-07-24 19:48:02 +0530

    Refactor socket connection in Home component to use secure WebSocket protocol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { logout, setUser , setOnlineUser, setSocketConnection } from "../stores/userSlice";
+import { logout, setUser, setOnlineUser, setSocketConnection } from "../stores/userSlice";
 import SidebarUser from "../components/SidebarUser";
 import logo from "../assets/Textora3.jpg";
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 
 function Home() {
   const user = useSelector((state) => state.user);
@@ -13,23 +13,18 @@ function Home() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // console.log(user)
   const fetchUserDetails = async () => {
     try {
       const url = `${import.meta.env.VITE_APP_BACKEND_URL}/api/user-data`;
       const response = await axios.get(url, {
-        url: url,
         withCredentials: true,
       });
-      // console.log("response",response)
       dispatch(setUser(response.data.data));
 
       if (response?.data?.data?.logout) {
         dispatch(logout());
         navigate("/email");
       }
-
-      // console.log('current user details:', response);
     } catch (error) {
       console.error("error:", error);
     }
@@ -41,28 +36,39 @@ function Home() {
 
   // socket connection
   useEffect(() => {
-    const socketUrl = import.meta.env.VITE_APP_BACKEND_URL.replace('http', 'ws');
-    console.log("socketUrl",socketUrl)
+    const socketUrl = import.meta.env.VITE_APP_BACKEND_URL.replace(/^http/, 'ws');
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('Token not found');
+      return;
+    }
+
     const socketConnection = io(socketUrl, {
       auth: {
-        token: localStorage.getItem('token'),
+        token: token,
       },
-    });
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
-      dispatch(setOnlineUser(data))
+      transports: ['websocket'], // Ensure only websocket transport is used
     });
 
-    dispatch(setSocketConnection(socketConnection))
+    socketConnection.on('connect_error', (err) => {
+      console.error('Socket connection error:', err);
+    });
+
+    socketConnection.on('onlineUser', (data) => {
+      console.log(data);
+      dispatch(setOnlineUser(data));
+    });
+
+    dispatch(setSocketConnection(socketConnection));
     return () => {
       socketConnection.disconnect();
     };
-  }, []);
-  
+  }, [dispatch]);
+
   const basePath = location.pathname === "/";
   return (
     <div className="grid lg:grid-cols-[380px,1fr] h-screen max-h-screen">
-      <section className={`bg-[#d1d8cd]  ${!basePath && "hidden"} lg:block`}>
+      <section className={`bg-[#d1d8cd] ${!basePath && "hidden"} lg:block`}>
         <SidebarUser />
       </section>
       {/* Message component  */}
@@ -70,16 +76,11 @@ function Home() {
         <Outlet />
       </section>
 
-      <div
-        className={`justify-center items-center flex-col hidden ${
-          !basePath ? "hidden" : "lg:flex"
-        }`}
-      >
+      <div className={`justify-center items-center flex-col hidden ${!basePath ? "hidden" : "lg:flex"}`}>
         <div className="blend">
           <img
             src={logo}
             alt="lgo"
-            // className='w-32 h-32 mx-auto mt-10'
             width={350}
           />
         </div>
